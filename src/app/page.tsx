380b1da41ab9c935a616c7443f5311fbf476a12e
Archit Kakkar <education.archit@gmail.com>
2024-06-02 18:03:38 +0530

    container added.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
   return (
-    <div className="text-4xl flex justify-center items-center h-screen w-full">
-      Hello World!
-    </div>
+    <MaxWidthWrapper className="py-5">
+      <p className="border rounded-xl p-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        nemo obcaecati quidem esse.
+      </p>
+    </MaxWidthWrapper>
   );
 }
