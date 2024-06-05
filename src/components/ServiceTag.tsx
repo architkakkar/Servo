3d4a46abe2d20447c383cbec57e13c2fd095a413
Archit Kakkar <education.archit@gmail.com>
2024-06-05 23:13:37 +0530

    shows business type over card.

---
+++ src/components/ServiceTag.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ServiceTagProps {
+  businessType: string;
+}
+
+function ServiceTag({ businessType }: ServiceTagProps) {
+  return (
+    <div className="rounded-full text-xs font-semibold absolute top-2 left-2 bg-black text-zinc-200 px-2 py-1">
+      {businessType}
+    </div>
+  );
+}
+
+export default ServiceTag;
