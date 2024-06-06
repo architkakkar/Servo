abf8ebe87b7c1b95d5ed088126fc7d4a6dedc1e1
Archit Kakkar <education.archit@gmail.com>
2024-06-06 23:29:49 +0530

    added a footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Luckiest_Guy } from "next/font/google";
+
+const luckiestGuy = Luckiest_Guy({ weight: "400", subsets: ["latin"] });
 
 function Footer() {
-  return <div className="text-white bg-black h-40 mb-18">Footer</div>;
+  return (
+    <footer className="pb-16 bg-black">
+      <div className="text-white bg-black text-sm flex flex-col items-center jusitify-center mb-1 py-4 gap-y-1">
+        <p className={`${luckiestGuy.className} text-2xl`}>Servo</p>
+        <p>@All Copyrights Reserved - 2024</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
