78d39c9ba75a286851246dde9a439c4809591e26
Archit Kakkar <education.archit@gmail.com>
2024-06-06 23:29:17 +0530

    removed favourites and added a cart option.

--- src/components/MainMenuButtons.tsx
+++ src/components/MainMenuButtons.tsx
@@ -3,37 +3,19 @@
 import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
 import { BiSolidCategory } from "react-icons/bi";
-import { FaHeart, FaUser } from "react-icons/fa6";
+import { FaCartShopping, FaUser } from "react-icons/fa6";
 
 function MainMenuButtons() {
   return (
-    <section className="bg-white/80 fixed bottom-0 w-full z-50 border-t-1.5 border-black rounded-t-xl md:hidden">
+    <section className="bg-white fixed bottom-0 w-full z-50 border-t-1.5 border-black rounded-t-xl md:hidden shadow-menuBtns">
       <ul className="grid grid-cols-4 items-center justify-center py-3">
         <li>
           <Link
             href="/"
-            className="flex flex-col items-center justify-center gap-y-1 hover:-translate-y-1 transition-all"
+            className="flex flex-col items-center justify-center gap-y-1"
           >
             <AiFillHome className="w-6 h-6" />
             <span className="text-xs font-semibold">Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/categories"
-            className="flex flex-col items-center justify-center gap-y-1"
-          >
-            <BiSolidCategory className="w-6 h-6" />
-            <span className="text-xs font-semibold">Categories</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/favourites"
-            className="flex flex-col items-center justify-center gap-y-1"
-          >
-            <FaHeart className="w-6 h-6" />
-            <span className="text-xs font-semibold">Favourites</span>
           </Link>
         </li>
         <li>
@@ -43,6 +25,24 @@ function MainMenuButtons() {
           >
             <FaUser className="w-6 h-6" />
             <span className="text-xs font-semibold">Account</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/category"
+            className="flex flex-col items-center justify-center gap-y-1"
+          >
+            <BiSolidCategory className="w-6 h-6" />
+            <span className="text-xs font-semibold">Categories</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/checkout"
+            className="flex flex-col items-center justify-center gap-y-1"
+          >
+            <FaCartShopping className="w-6 h-6" />
+            <span className="text-xs font-semibold">Cart</span>
           </Link>
         </li>
       </ul>
