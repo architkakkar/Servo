f3a5bd32dc74e584137451c46690d7f03b656400
Archit Kakkar <education.archit@gmail.com>
2024-06-12 03:00:18 +0530

    Added price field.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,9 +12,18 @@ interface CardProps {
   stars: number;
   reviews: number;
   services: string[];
+  price: number;
 }
 
-function Card({ title, image, type, stars, reviews, services }: CardProps) {
+function Card({
+  title,
+  image,
+  type,
+  stars,
+  reviews,
+  services,
+  price,
+}: CardProps) {
   const printServices = (services: string[]) => {
     const len = services.length;
     let servicesString = "";
@@ -49,28 +58,34 @@ function Card({ title, image, type, stars, reviews, services }: CardProps) {
           <IoHeartOutline className="h-6 w-6" />
         </div>
       </div>
-      {/* Business Description */}
-      <div className="overflow-hidden p-3 flex flex-col gap-y-1">
-        {/* Business Name */}
-        <h1 className="font-extrabold text-sm">{title}</h1>
-        {/* Stars & Reviews */}
-        <div className="flex items-center justify-start text-xs font-semibold gap-x-1">
-          <div className="flex justify-center items-center bg-black text-zinc-200 px-1.5 py-0.5 rounded-md gap-x-1">
-            <FaStar className="w-4 h-4 -translate-y-[0.5px]" />
-            <span>{stars}</span>
+      {/* Busin ess Description */}
+      <div className="p-3 flex flex-col justify-between relative">
+        <div className="overflow-hidden flex flex-col gap-y-1 grow h-full">
+          {/* Business Name */}
+          <h1 className="font-extrabold text-[15px] mb-0.5 leading-snug">
+            {title}
+          </h1>
+          {/* Stars & Reviews */}
+          <div className="flex items-center justify-start text-xs font-semibold gap-x-1">
+            <div className="flex justify-center items-center bg-black text-zinc-200 px-1.5 py-0.5 rounded-md gap-x-1">
+              <FaStar className="w-4 h-4 -translate-y-[0.5px]" />
+              <span>{stars}</span>
+            </div>
+            <GoDotFill className="w-3 h-3" />
+            <span className="whitespace-nowrap overflow-hidden text-ellipsis">{`${reviews} Reviews`}</span>
           </div>
-          <GoDotFill className="w-3 h-3" />
-          <span className="whitespace-nowrap overflow-hidden text-ellipsis">{`${reviews} Reviews`}</span>
+          {/* Popular Services Offered */}
+          <div className="overflow-hidden">
+            <p className="whitespace-nowrap text-xs text-zinc-600 overflow-hidden text-ellipsis">
+              {printServices(services)}
+            </p>
+          </div>
+          {/* Price */}
+          <div className="font-extrabold text-xl mt-0.5 mb-2 font-sans">{`Rs. ${price}/-`}</div>
+          {/* Book Now Button */}
         </div>
-        {/* Popular Services Offered */}
-        <div className="overflow-hidden">
-          <p className="whitespace-nowrap text-xs text-zinc-600 overflow-hidden text-ellipsis">
-            {printServices(services)}
-          </p>
-        </div>
-        {/* Book Now Button */}
-        <Link href={"/"}>
-          <div className="bg-black text-zinc-200 font-bold capitialize rounded-xl w-full flex items-center justify-center mt-2 text-sm hover:opacity-90">
+        <Link href={"/"} className="flex-">
+          <div className="bg-black text-zinc-200 font-bold capitialize rounded-lg w-full flex items-center justify-center text-sm hover:opacity-90">
             <button className="text-center py-2">Book Now</button>
           </div>
         </Link>
