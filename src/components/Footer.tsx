1b235d7a3a7efdeb47abbe301e8aeac1c2f7ac74
Archit Kakkar <education.archit@gmail.com>
2024-06-14 19:50:35 +0530

    wrapped footer into MaxWidthWrapper.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Luckiest_Guy } from "next/font/google";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const luckiestGuy = Luckiest_Guy({ weight: "400", subsets: ["latin"] });
 
 function Footer() {
   return (
-    <footer className="pb-16 bg-black">
-      <div className="text-white bg-black text-sm flex flex-col items-center jusitify-center mb-1 py-4 gap-y-1">
-        <p className={`${luckiestGuy.className} text-2xl`}>Servo</p>
-        <p>@All Copyrights Reserved - 2024</p>
-      </div>
-    </footer>
+    <MaxWidthWrapper className="!px-0">
+      <footer className="pb-16 md:pb-2 bg-black">
+        <div className="text-white bg-black text-sm flex flex-col items-center jusitify-center mb-1 py-4 md:py-6 gap-y-1">
+          <p className={`${luckiestGuy.className} text-2xl md:text-3xl`}>
+            Servo
+          </p>
+          <p>@All Copyrights Reserved - 2024</p>
+        </div>
+      </footer>
+    </MaxWidthWrapper>
   );
 }
 
