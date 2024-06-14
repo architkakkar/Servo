5a28085c9c1bbc8bb363ab76cc49cc5ddfbd2dc7
Archit Kakkar <education.archit@gmail.com>
2024-06-14 20:06:29 +0530

    made it fully responsive.

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Luckiest_Guy } from "next/font/google";
 import { TbGhost2Filled } from "react-icons/tb";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 const luckiestGuy = Luckiest_Guy({ weight: "400", subsets: ["latin"] });
 
@@ -12,16 +13,17 @@ function Checkout() {
 
   return (
     <>
+      <Navbar className="hidden md:block" />
       {isCartEmpty ? (
         <div
           className={`${luckiestGuy.className} flex items-center justify-center h-[calc(100vh-4rem)] w-full`}
         >
-          <div className="text-center text-neutral-600 text-sm uppercase flex items-center justify-center flex-col">
-            <TbGhost2Filled className="w-6 h-6 mb-1" />
-            <p>NOTHING TO SHOW</p>
-            <p>EXPLORE POPULAR SERVICES NOW!</p>
+          <div className="text-center text-zinc-600/75 text-sm uppercase flex items-center justify-center flex-col">
+            <TbGhost2Filled className="w-8 h-8 mb-1" />
+            <p className="uppercase">Nothing to show</p>
+            <p className="uppercase">Explore popular services now!</p>
             <Link href="/category">
-              <button className="bg-black hover:black/90 text-zinc-200 px-4 py-2 mt-2 rounded-md tracking-wide">
+              <button className="bg-black hover:bg-black/90 text-zinc-200 px-4 py-2 mt-2 rounded-md tracking-wide">
                 Explore
               </button>
             </Link>
