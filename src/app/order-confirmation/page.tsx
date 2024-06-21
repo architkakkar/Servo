1ec3a6bc272c3f3907dc1f774b75f63aad376044
Archit Kakkar <education.archit@gmail.com>
2024-06-21 20:33:07 +0530

    enclosed within a container with border.

--- src/app/order-confirmation/page.tsx
+++ src/app/order-confirmation/page.tsx
@@ -10,11 +10,13 @@ function page() {
       <Link href="/" className="absolute top-6 right-6 cursor-pointer">
         <CgCloseO className="text-white w-7 h-7" />
       </Link>
-      <div className="flex flex-col items-center justify-center h-full space-y-2 md:space-y-4 pb-16">
-        <FaCircleCheck className="text-green-200 border-green-600 bg-green-600 rounded-full w-10 h-10" />
-        <div className="text-center text-green-300/95">
-          <p>Thank You!</p>
-          <p>Your Booking has been confirmed</p>
+      <div className="flex items-center justify-center h-full pb-16">
+        <div className="border-2 border-green-300/95 rounded-xl p-10 flex flex-col items-center justify-center space-y-2 md:space-y-4">
+          <FaCircleCheck className="text-green-200 border-green-600 bg-green-600 rounded-full w-10 h-10" />
+          <div className="text-center text-green-300/95">
+            <p>Thank You!</p>
+            <p>Your Booking has been confirmed</p>
+          </div>
         </div>
       </div>
       <p className="text-white/75 absolute right-1/2 translate-x-1/2 bottom-24 md:bottom-10 font-mono text-sm ">
